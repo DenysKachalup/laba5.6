293e7e1efcc800fdc5c90f6e297a0fda10311e9a
Denys Kachalup <denys-maksym.kachalup.itis.2020@lpnu.ua>
2020-11-09 20:05:08 +0200

    laba5.6

---
+++ laba5.6/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int sum(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int a, b;
+	cout << "a = ? "; cin >> a;
+	cout << "b = ? "; cin >> b;
+	int c = sum(a, b);
+	cout << "c = " << c << endl;
+	system("pause");
+	return 0;
